78c1f253b0708a6b4b8080d66cc70235a0b84bde
DanRoss88 <danielmartinross@gmail.com>
2023-07-25 02:05:41 -0700

    routes working

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,44 +1,99 @@
+// const User = require("../models/user");
+// const { createSecretToken } = require("../util/secretToken");
+// const bcrypt = require("bcrypt");
+
+// module.exports.Register = async (req, res, next) => {
+//   try {
+//     const {
+//       full_name,
+//       location,
+//       renter,
+//       owner,
+//       email,
+//       password,
+//       username,
+//       website,
+//       bio,
+//       phone,
+//       verified,
+//       verified_doc,
+//       agreement,
+//       createdAt,
+//     } = req.body;
+//     const existingUser = await User.findOne({ email });
+//     if (existingUser) {
+//       return res.json({ message: "User already exists" });
+//     }
+//     const user = await User.create({ 
+//       full_name,
+//       location,
+//       renter,
+//       owner,
+//       email,
+//       password,
+//       username,
+//       website,
+//       bio,
+//       phone,
+//       verified,
+//       verified_doc,
+//       agreement,
+//       createdAt });
+//     const token = createSecretToken(user._id);
+//     res.cookie("token", token, {
+//       withCredentials: true,
+//       httpOnly: false,
+//     });
+//     res
+//       .status(201)
+//       .json({ message: "User signed in successfully", success: true, user });
+//     next();
+//   } catch (error) {
+//     console.error(error);
+//     return res.status(500).json({ message: "Internal server error" });
+//   }
+// };
+
+// module.exports.Login = async (req, res, next) => {
+//     try {
+//       const {username, password } = req.body;
+//       if(!username || !password) {
+//         return res.status(400).json({ message: "Please enter all required fields" });
+//       }
+//       const user = await User.findOne({ username });
+//       if(!user) {
+//         return res.status(400).json({ message: "User does not exist" });
+//       }
+//       const auth = await bcrypt.compare(password, user.password);
+//       if(!auth) {
+//         return res.status(400).json({ message: "Incorrect password" });
+//       }
+//       const token = createSecretToken(user._id);
+//       res.cookie("token", token, {
+//         withCredentials: true,
+//         httpOnly: false,
+//       });
+//       res.status(200).json({ message: "User signed in successfully", success: true, user });
+//       next();
+//     } catch (error) {
+//       console.error(error);
+//       return res.status(500).json({ message: "Internal server error" });
+//     }
+//     };
+
+
 const User = require("../models/user");
 const { createSecretToken } = require("../util/secretToken");
 const bcrypt = require("bcrypt");
 
 module.exports.Register = async (req, res, next) => {
   try {
-    const {
-      full_name,
-      location,
-      renter,
-      owner,
-      email,
-      password,
-      username,
-      website,
-      bio,
-      phone,
-      verified,
-      verified_doc,
-      agreement,
-      createdAt,
-    } = req.body;
+    const { email, password, username, createdAt } = req.body;
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.json({ message: "User already exists" });
     }
-    const user = await User.create({ 
-      full_name,
-      location,
-      renter,
-      owner,
-      email,
-      password,
-      username,
-      website,
-      bio,
-      phone,
-      verified,
-      verified_doc,
-      agreement,
-      createdAt });
+    const user = await User.create({ email, password, username, createdAt });
     const token = createSecretToken(user._id);
     res.cookie("token", token, {
       withCredentials: true,
@@ -50,33 +105,31 @@ module.exports.Register = async (req, res, next) => {
     next();
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ message: "Internal server error" });
   }
 };
 
 module.exports.Login = async (req, res, next) => {
-    try {
-      const {username, password } = req.body;
-      if(!username || !password) {
-        return res.status(400).json({ message: "Please enter all required fields" });
-      }
-      const user = await User.findOne({ username });
-      if(!user) {
-        return res.status(400).json({ message: "User does not exist" });
-      }
-      const auth = await bcrypt.compare(password, user.password);
-      if(!auth) {
-        return res.status(400).json({ message: "Incorrect password" });
-      }
-      const token = createSecretToken(user._id);
-      res.cookie("token", token, {
-        withCredentials: true,
-        httpOnly: false,
-      });
-      res.status(200).json({ message: "User signed in successfully", success: true, user });
-      next();
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({ message: "Internal server error" });
+  try {
+    const { email, password } = req.body;
+    if(!email || !password ){
+      return res.json({message:'All fields are required'})
     }
-    };
+    const user = await User.findOne({ email });
+    if(!user){
+      return res.json({message:'Incorrect password or email' }) 
+    }
+    const auth = await bcrypt.compare(password,user.password)
+    if (!auth) {
+      return res.json({message:'Incorrect password or email' }) 
+    }
+     const token = createSecretToken(user._id);
+     res.cookie("token", token, {
+       withCredentials: true,
+       httpOnly: false,
+     });
+     res.status(201).json({ message: "User logged in successfully", success: true });
+     next()
+  } catch (error) {
+    console.error(error);
+  }
+}
--- src/models/user.js
+++ src/models/user.js
@@ -1,93 +1,122 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+// const mongoose = require('mongoose');
+// const bcrypt = require('bcrypt');
 
-const saltRounds = 12; // Number of salt rounds
+// const saltRounds = 12; // Number of salt rounds
+
+// const userSchema = new mongoose.Schema({
+//   full_name: { 
+//     type: String,
+//     required: [true, 'Please enter your full name'],
+//   },
+//   username:{
+//     type: String,
+//     required: [true, 'Please enter a username'],
+//     unique: true,
+//   },
+//   rating:{
+//     type: Number,
+//     default: 0,
+//   },
+//   profile_pic: {
+//     type: String,
+//   },
+//   email: { 
+//     type: String,
+//     required: [true, 'Please enter an email'],
+//     unique: true,
+//   },
+//   password: {
+//     type: String,
+//     required: [true, 'Please enter a password'],
+//     minlength: [6, 'Minimum password length is 6 characters'],
+//   },
+//   location: { 
+//     type:String,
+//     required: [true, 'Please enter your location'],
+//   },
+//   owner: {
+//     type: Boolean,
+//     default: false,
+//     required: [true, 'Please enter your status'],
+//   },
+//   renter: {
+//     type: Boolean,
+//     default: false,
+//     required: [true, 'Please enter your status'],
+//   },
+//   website: {
+//     type: String,
+//   },
+//   phone: {
+//     type: String,
+//     required: [true, 'Please enter your phone number'],
+//   },
+//   bio: {
+//     type: String,
+//     required: [true, 'Please enter a bio'],
+//     minlength: [10, 'Minimum bio length is 10 characters'],
+//   },
+//   verified: {
+//     type: Boolean,
+//     default: false,
+//   },
+//   verified_doc_id: 
+//   { type: mongoose.Schema.Types.ObjectId, 
+//     ref: 'verified_doc',
+//   },
+//   agreement: {
+//     type: Boolean,
+//     default: false,
+//     required: [true, 'Please check the agreement'],
+//   },
+//   created_at: { 
+//     type: Date, 
+//     default: Date.now 
+//   },
+// });
+
+// userSchema.pre('save', async function (next) {
+//   try {
+//     if (!this.isModified('password')) {
+//       return next(); // Skip hashing if the password is not modified (e.g., during update)
+//     }
+//     const salt = await bcrypt.genSalt(saltRounds); // Generate the salt
+//     this.password = await bcrypt.hash(this.password, salt); // Hash the password with the salt
+//     return next();
+//   } catch (error) {
+//     return next(error); // Call the next middleware with the error
+//   }
+// });
+
+// const User = mongoose.model('User', userSchema);
+
+// module.exports = User;
+
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
-  full_name: { 
+  email: {
     type: String,
-    required: [true, 'Please enter your full name'],
-  },
-  username:{
-    type: String,
-    required: [true, 'Please enter a username'],
+    required: [true, "Your email address is required"],
     unique: true,
   },
-  rating:{
-    type: Number,
-    default: 0,
-  },
-  profile_pic: {
+  username: {
     type: String,
-  },
-  email: { 
-    type: String,
-    required: [true, 'Please enter an email'],
-    unique: true,
+    required: [true, "Your username is required"],
   },
   password: {
     type: String,
-    required: [true, 'Please enter a password'],
-    minlength: [6, 'Minimum password length is 6 characters'],
+    required: [true, "Your password is required"],
   },
-  location: { 
-    type:String,
-    required: [true, 'Please enter your location'],
-  },
-  owner: {
-    type: Boolean,
-    default: false,
-    required: [true, 'Please enter your status'],
-  },
-  renter: {
-    type: Boolean,
-    default: false,
-    required: [true, 'Please enter your status'],
-  },
-  website: {
-    type: String,
-  },
-  phone: {
-    type: String,
-    required: [true, 'Please enter your phone number'],
-  },
-  bio: {
-    type: String,
-    required: [true, 'Please enter a bio'],
-    minlength: [10, 'Minimum bio length is 10 characters'],
-  },
-  verified: {
-    type: Boolean,
-    default: false,
-  },
-  verified_doc_id: 
-  { type: mongoose.Schema.Types.ObjectId, 
-    ref: 'verified_doc',
-  },
-  agreement: {
-    type: Boolean,
-    default: false,
-    required: [true, 'Please check the agreement'],
-  },
-  created_at: { 
-    type: Date, 
-    default: Date.now 
+  createdAt: {
+    type: Date,
+    default: new Date(),
   },
 });
 
-userSchema.pre('save', async function (next) {
-  try {
-    if (!this.isModified('password')) {
-      return next(); // Skip hashing if the password is not modified (e.g., during update)
-    }
-    const salt = await bcrypt.genSalt(saltRounds); // Generate the salt
-    this.password = await bcrypt.hash(this.password, salt); // Hash the password with the salt
-    return next();
-  } catch (error) {
-    return next(error); // Call the next middleware with the error
-  }
+userSchema.pre("save", async function () {
+  this.password = await bcrypt.hash(this.password, 12);
 });
 
-const User = mongoose.model('User', userSchema);
-
-module.exports = User;
+module.exports = mongoose.model("User", userSchema);
